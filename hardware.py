0a683d311c6f566a700eb28b40787595a619421f
Dan Yang <dsyang92@gmail.com>
2021-10-11 08:32:54 +0100

    add a file to communicate with arduino over serial usb

---
+++ hardware.py
@@ -0,0 +1,16 @@
+import logging
+import time
+import serial
+
+def start_watering(zone_num):
+    pass
+
+def stop_watering(zone_num):
+    pass
+
+def read_moisture_sensors():
+    pass
+
+def init():
+    # initialize serial USB connection with Arduino
+    pass
