42cadf099719aba9826f125da49afc3b1845d290
Dan Yang <dsyang92@gmail.com>
2021-10-17 22:56:23 +0100

    update arduino code to adhere to spec

--- nessie_watering/nessie_watering.ino
+++ nessie_watering/nessie_watering.ino
@@ -13,7 +13,7 @@ From https://cpp4arduino.com/2020/02/07/how-to-format-strings-without-the-string
 
 */
 /* 0 = none 1 = normal 2 = verbose */
-int IS_DEBUG_LEVEL = 2;
+int IS_DEBUG_LEVEL = 0;
 
 int NUM_ZONES = 4;
 int MOISTURE_SENSOR_ZONE_0 = A0;
@@ -21,17 +21,44 @@ int MOISTURE_SENSOR_ZONE_1 = A2;
 int MOISTURE_SENSOR_ZONE_2 = A4;
 int MOISTURE_SENSOR_ZONE_3 = A5;
 
+int FRESHLY_WATERED_PLANT_READING = 700;
+int AIR_READING = 810;
 int MOISTURE_SENSORS[] = {MOISTURE_SENSOR_ZONE_0, MOISTURE_SENSOR_ZONE_1, MOISTURE_SENSOR_ZONE_2, MOISTURE_SENSOR_ZONE_3};
 char SENSE_CMD_OUT[] = "%04d %04d %04d %04d";
 
-
+int SOLENOID_VALVE_PIN = 7;
 int WATER_PUMP_ZONE_0 = 7;
 int WATER_PUMP_ZONE_1 = 6;
 int WATER_PUMP_ZONE_2 = 4;
 int WATER_PUMP_ZONE_3 = 5;
 
-int ZONE_PINS[] = { WATER_PUMP_ZONE_0, WATER_PUMP_ZONE_1, WATER_PUMP_ZONE_2, WATER_PUMP_ZONE_3};
+int ZONE_PINS[] = {WATER_PUMP_ZONE_0, WATER_PUMP_ZONE_1, WATER_PUMP_ZONE_2, WATER_PUMP_ZONE_3};
 char STATUS_CMD_OUT[] = "Z0(%d)=%d:%d Z1(%d)=%d:%d Z2(%d)=%d:%d Z3(%d)=%d:%d";
+
+char RESPONSE_JSON_OUT_PREFIX[] = "{\"data\":";
+char RESPONSE_JSON_OUT_POSTFIX[] = "}\n";
+char CONIFG_JSON_OUT[] = "{ \
+\"debug_level\":%d, \
+\"num_relay_pins\": %d, \
+\"solenoid_pin\": %d, \
+\"num_sensors\": %d, \
+\"sensor_wet\": %d, \
+\"sensor_dry\": %d \
+}";
+
+char STATUS_JSON_OUT[] = "{ \
+\"%d\": [ %d, %d], \
+\"%d\": [ %d, %d], \
+\"%d\": [ %d, %d], \
+\"%d\": [ %d, %d] \
+}";
+
+char SENSE_JSON_OUT[] = "{ \
+[ %04d, %04d, %04d, %04d] \
+}";
+
+char ZONE_WATER_SUCCESS[] = "200";
+char ZONE_WATER_NO_CHANGE[] = "304";
 
 // High means LED is off
 int zoneStates[] = {HIGH, HIGH, HIGH, HIGH};
@@ -39,7 +66,8 @@ int zoneStates[] = {HIGH, HIGH, HIGH, HIGH};
 String in_buffer;
 String out_buffer;
 
-void setup() {
+void setup()
+{
   analogReference(EXTERNAL);
 
   pinMode(WATER_PUMP_ZONE_0, OUTPUT);
@@ -55,64 +83,100 @@ void setup() {
   digitalWrite(WATER_PUMP_ZONE_3, zoneStates[3]);
 
   Serial.begin(9600);
-  while (!Serial) {
+  while (!Serial)
+  {
     // Wait for serial connection to be established.
   }
-  if (IS_DEBUG_LEVEL) {
+  if (IS_DEBUG_LEVEL)
+  {
     Serial.setTimeout(5000);
   }
 }
 
-void loop() {
+void loop()
+{
   readSerialPortUntil();
 
-  if (in_buffer != "") {
-    if (in_buffer == "SENSE") {
+  if (in_buffer != "")
+  {
+    if (in_buffer == "DBG0") {
+      IS_DEBUG_LEVEL = 0;
+      writeConfig();
+    } else if (in_buffer == "DBG1") {
+      IS_DEBUG_LEVEL = 1;
+      writeConfig();
+    } else if (in_buffer == "DBG2") {
+      IS_DEBUG_LEVEL = 2;
+      writeConfig();
+    } else if (in_buffer == "CONFIG") {
+      writeConfig();
+    }
+
+    if (in_buffer == "SENSE")
+    {
       readMoistureSensors();
-
-    } else if (in_buffer == "STATUS") {
+    }
+    else if (in_buffer == "STATUS")
+    {
       readZones();
-
-    } else if (in_buffer == "ZONE0_0") {
+    }
+    else if (in_buffer == "ZONE0_0")
+    {
       zoneStates[0] = water(WATER_PUMP_ZONE_0, zoneStates[0], LOW);
-    } else if (in_buffer == "ZONE0_1") {
+    }
+    else if (in_buffer == "ZONE0_1")
+    {
       zoneStates[0] = water(WATER_PUMP_ZONE_0, zoneStates[0], HIGH);
     }
 
-     else if (in_buffer == "ZONE1_0") {
+    else if (in_buffer == "ZONE1_0")
+    {
       zoneStates[1] = water(WATER_PUMP_ZONE_1, zoneStates[1], LOW);
-    } else if (in_buffer == "ZONE1_1") {
+    }
+    else if (in_buffer == "ZONE1_1")
+    {
       zoneStates[1] = water(WATER_PUMP_ZONE_1, zoneStates[1], HIGH);
     }
 
-     else if (in_buffer == "ZONE2_0") {
+    else if (in_buffer == "ZONE2_0")
+    {
       zoneStates[2] = water(WATER_PUMP_ZONE_2, zoneStates[2], LOW);
-    } else if (in_buffer == "ZONE2_1") {
+    }
+    else if (in_buffer == "ZONE2_1")
+    {
       zoneStates[2] = water(WATER_PUMP_ZONE_2, zoneStates[2], HIGH);
     }
 
-     else if (in_buffer == "ZONE3_0") {
+    else if (in_buffer == "ZONE3_0")
+    {
       zoneStates[3] = water(WATER_PUMP_ZONE_3, zoneStates[3], LOW);
-    } else if (in_buffer == "ZONE3_1") {
+    }
+    else if (in_buffer == "ZONE3_1")
+    {
       zoneStates[3] = water(WATER_PUMP_ZONE_3, zoneStates[3], HIGH);
-    } else if (IS_DEBUG_LEVEL > 1) {
-      if (in_buffer == "z1") {
-	zoneStates[1] = water(WATER_PUMP_ZONE_1, zoneStates[1], zoneStates[1] == LOW ? HIGH : LOW);
-      } else if (in_buffer == "z2") {
-	zoneStates[2] = water(WATER_PUMP_ZONE_2, zoneStates[2], zoneStates[2] == LOW ? HIGH : LOW);
-
-      } else if (in_buffer == "z3") {
-	zoneStates[3] = water(WATER_PUMP_ZONE_3, zoneStates[3], zoneStates[3] == LOW ? HIGH : LOW);
-      
-      } else if (in_buffer == "z0") {
-	zoneStates[0] = water(WATER_PUMP_ZONE_0, zoneStates[0], zoneStates[0] == LOW ? HIGH : LOW);
+    }
+    else if (IS_DEBUG_LEVEL > 1)
+    {
+      if (in_buffer == "z1")
+      {
+        zoneStates[1] = water(WATER_PUMP_ZONE_1, zoneStates[1], zoneStates[1] == LOW ? HIGH : LOW);
       }
-
+      else if (in_buffer == "z2")
+      {
+        zoneStates[2] = water(WATER_PUMP_ZONE_2, zoneStates[2], zoneStates[2] == LOW ? HIGH : LOW);
+      }
+      else if (in_buffer == "z3")
+      {
+        zoneStates[3] = water(WATER_PUMP_ZONE_3, zoneStates[3], zoneStates[3] == LOW ? HIGH : LOW);
+      }
+      else if (in_buffer == "z0")
+      {
+        zoneStates[0] = water(WATER_PUMP_ZONE_0, zoneStates[0], zoneStates[0] == LOW ? HIGH : LOW);
+      }
     }
 
-
-
-    if(out_buffer != "" && IS_DEBUG_LEVEL > 1) {
+    if (out_buffer != "" && IS_DEBUG_LEVEL > 1)
+    {
       writeSerialPort();
       delay(10);
       readZones();
@@ -122,7 +186,24 @@ void loop() {
   writeSerialPort();
 
   delay(500);
+}
 
+void writeConfig()
+{
+  char resp[200];
+  snprintf(
+      resp,
+      sizeof(resp),
+      CONIFG_JSON_OUT,
+      IS_DEBUG_LEVEL,
+      NUM_ZONES,
+      SOLENOID_VALVE_PIN,
+      NUM_ZONES,
+      FRESHLY_WATERED_PLANT_READING,
+      AIR_READING);
+  out_buffer += RESPONSE_JSON_OUT_PREFIX;
+  out_buffer += resp;
+  out_buffer += RESPONSE_JSON_OUT_POSTFIX;
 }
 
 /**
@@ -131,25 +212,33 @@ void loop() {
  * 700-710 - Freshly Watered Plant
  * 500-570 - In Water
  */
-void readMoistureSensors() {
+void readMoistureSensors()
+{
   int SAMPLES_PER_SENSOR = 5;
   int readings[NUM_ZONES] = {0, 0, 0, 0};
-  for(int sensor = 0; sensor < NUM_ZONES; sensor++) {
+  for (int sensor = 0; sensor < NUM_ZONES; sensor++)
+  {
     readings[sensor] = readSingleMoistureSensor(sensor, MOISTURE_SENSORS[sensor], SAMPLES_PER_SENSOR);
   }
   char resp[100];
 
-  snprintf(resp, sizeof(resp), SENSE_CMD_OUT, readings[0], readings[1], readings[2], readings[3]);
+  snprintf(resp, sizeof(resp), SENSE_JSON_OUT, readings[0], readings[1], readings[2], readings[3]);
+
+  out_buffer += RESPONSE_JSON_OUT_PREFIX;
   out_buffer += resp;
+  out_buffer += RESPONSE_JSON_OUT_POSTFIX;
 }
 
-int readSingleMoistureSensor(int sensor_num, int pin, int num_samples) {
+int readSingleMoistureSensor(int sensor_num, int pin, int num_samples)
+{
   int reading = 0;
   int samples[num_samples];
 
-  for (int i = 0; i < num_samples; i++) {
+  for (int i = 0; i < num_samples; i++)
+  {
     samples[i] = analogRead(pin);
-    if (IS_DEBUG_LEVEL > 1) {
+    if (IS_DEBUG_LEVEL > 1)
+    {
       char resp[100];
       snprintf(resp, sizeof(resp), "Reading sensor %d(%d): %04d", sensor_num, pin, samples[i]);
       Serial.println(resp);
@@ -158,7 +247,8 @@ int readSingleMoistureSensor(int sensor_num, int pin, int num_samples) {
   }
 
   int avg;
-  for (int j = 0; j < num_samples; j++) {
+  for (int j = 0; j < num_samples; j++)
+  {
     avg += samples[j];
   }
   avg /= num_samples;
@@ -169,73 +259,67 @@ int readSingleMoistureSensor(int sensor_num, int pin, int num_samples) {
 /**
  * Reads the zone state and the current pin state.
  */
-void readZones() {
+void readZones()
+{
   int currentPinVal[NUM_ZONES];
-  for (int i = 0; i < NUM_ZONES; i++) {
+  for (int i = 0; i < NUM_ZONES; i++)
+  {
     currentPinVal[i] = digitalRead(ZONE_PINS[i]);
   }
 
-  char resp[100];
+  char resp[200];
   snprintf(
-    resp, 
-    sizeof(resp),
-    STATUS_CMD_OUT,
-    ZONE_PINS[0], zoneStates[0], currentPinVal[0],
-    ZONE_PINS[1], zoneStates[1], currentPinVal[1],
-    ZONE_PINS[2], zoneStates[2], currentPinVal[2],
-    ZONE_PINS[3], zoneStates[3], currentPinVal[3]);
+      resp,
+      sizeof(resp),
+      STATUS_JSON_OUT,
+      ZONE_PINS[0], zoneStates[0], currentPinVal[0],
+      ZONE_PINS[1], zoneStates[1], currentPinVal[1],
+      ZONE_PINS[2], zoneStates[2], currentPinVal[2],
+      ZONE_PINS[3], zoneStates[3], currentPinVal[3]);
+
+  out_buffer += RESPONSE_JSON_OUT_PREFIX;
   out_buffer += resp;
+  out_buffer += RESPONSE_JSON_OUT_POSTFIX;
 }
 
 /**
  * Returns the value to store in the pint state.
  */
-int water(int pin, int currentPinState, int newPinState) {
+int water(int pin, int currentPinState, int newPinState)
+{
   int current_val = digitalRead(pin);
-  if (current_val == newPinState || currentPinState == newPinState) {
-    out_buffer = "304";
-    if (IS_DEBUG_LEVEL) {
-      writeSerialPort();
-      delay(10);
-      readZones();
-    }
+  if (current_val == newPinState || currentPinState == newPinState)
+  {
+    out_buffer += RESPONSE_JSON_OUT_PREFIX;
+    out_buffer += ZONE_WATER_NO_CHANGE;
+    out_buffer += RESPONSE_JSON_OUT_POSTFIX;
     return currentPinState;
-  } else {
+  }
+  else
+  {
     digitalWrite(pin, newPinState);
-    out_buffer = "200";
-    if (IS_DEBUG_LEVEL) {
-      writeSerialPort();
-      delay(10);
-      readZones();
-    }
+    out_buffer += RESPONSE_JSON_OUT_PREFIX;
+    out_buffer += ZONE_WATER_SUCCESS;
+    out_buffer += RESPONSE_JSON_OUT_POSTFIX;
     return newPinState;
   }
 }
 
-
-void readSerialPort() {
-  in_buffer = "";
-  if (Serial.available()) {
-    delay(10);
-    while (Serial.available() > 0) {
-      in_buffer += (char)Serial.read();
-    }
-    Serial.flush();
-  }
-}
-
-void readSerialPortUntil() {
+void readSerialPortUntil()
+{
   in_buffer = "";
   in_buffer = Serial.readStringUntil('|');
-  if (in_buffer != "" && IS_DEBUG_LEVEL) {
-    Serial.println("RECV: "+ in_buffer);
+  if (in_buffer != "" && IS_DEBUG_LEVEL)
+  {
+    Serial.println("RECV: " + in_buffer);
   }
 }
 
-void writeSerialPort() {
-  if (out_buffer != "") {
+void writeSerialPort()
+{
+  if (out_buffer != "")
+  {
     Serial.println(out_buffer);
     out_buffer = "";
   }
 }
-
